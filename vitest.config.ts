14648bd0c7d25525e2a536cbb06894e03d146681
lightd9 <aolafenwa@gmail.com>
2026-09-17 17:06:20 +0100

    chore: restore default vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
     globals: true,
     setupFiles: ['./src/test/setup.ts'],
     include: ['src/**/*.test.{ts,tsx}'],
-    poolTimeout: 600000,
   },
   resolve: {
     alias: {
